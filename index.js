aaad3d998097a0ec469e602b295491cdbd1af7a0
Vitali Semashkevich <vit-sema@yandex.ru>
2022-02-25 10:34:58 +0300

    [dev] add move piece methods

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-import Game from './src/Game';
+import Game from './src/game.js';
 
 const game = new Game();
+
+window.game = game;
+
+
+for (let index = 0; index < 25; index++) {
+    game.movePieceDown();
+    
+}
+
+
+console.log(game.board);
--- src/Game.js
+++ src/Game.js
@@ -3,15 +3,94 @@ export default class Game {
 		this.score = 0;
 		this.lines = 0;
 		this.level = 0;
-		this.board = [];
+		this.board = [
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		];
 		this.active = {
 			x: 0,
 			y: 0,
-			block: [
+			blocks: [
 				[0, 1, 0],
-				[0, 1, 0],
+				[1, 1, 1],
 				[0, 0, 0],
 			],
 		};
+	}
+
+	movePieceLeft() {
+		this.active.x -= 1;
+
+		if (this.hasCollision()) {
+			this.active.x += 1;
+			this.lockPiece();
+		}
+	}
+	movePieceRight() {
+		this.active.x += 1;
+
+		if (this.hasCollision()) {
+			this.active.x -= 1;
+			this.lockPiece();
+		}
+	}
+
+	movePieceDown() {
+		this.active.y += 1;
+
+		if (this.hasCollision()) {
+			this.active.y -= 1;
+			this.lockPiece();
+		}
+	}
+
+	hasCollision() {
+		const { active, board } = this;
+		const { x: pieceX, y: pieceY, blocks } = active;
+
+		for (let y = 0; y < blocks.length; y++) {
+			for (let x = 0; x < blocks[y].length; x++) {
+				if (
+					blocks[y][x] &&
+					(board[pieceY + y] === undefined ||
+						board[pieceY + y][pieceX + x] === undefined ||
+						board[pieceY + y][pieceX + x])
+				)
+					return true;
+			}
+		}
+
+		return false;
+	}
+
+	lockPiece() {
+		const { active, board } = this;
+		const { x: pieceX, y: pieceY, blocks } = active;
+
+		for (let y = 0; y < blocks.length; y++) {
+			for (let x = 0; x < blocks[y].length; x++) {
+				if (blocks[y][x]) {
+					board[pieceY + y][pieceX + x] = blocks[y][x];
+				}
+			}
+		}
 	}
 }
